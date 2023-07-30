62634e50aba11a943e580d3b16d441b880b791c4
Jesher <jesherjesher1@gmail.com>
2023-07-30 11:01:50 -0600

    13 vestidos terminado

--- app/models/vestidos.py
+++ app/models/vestidos.py
@@ -1,4 +1,5 @@
 from .db import get_connection
+import os
 
 def agregar_vestido(nombre, talla, color, categoria, descripcion, precio, estado, cantidad, img_vestido):
     mydb = get_connection()
@@ -16,7 +17,7 @@ def agregar_vestido(nombre, talla, color, categoria, descripcion, precio, estado
 def obtener_todos_los_vestidos():
     mydb = get_connection()
     cursor = mydb.cursor()
-    consulta = "SELECT * FROM productos"
+    consulta = "SELECT * FROM productos WHERE estado = 'Disponible'"
     cursor.execute(consulta)
 
     vestidos = cursor.fetchall()
@@ -24,3 +25,92 @@ def obtener_todos_los_vestidos():
     cursor.close()
     mydb.close()
     return vestidos
+
+def eliminar_vestido_por_id(producto_id):
+    mydb = get_connection()
+    cursor = mydb.cursor()
+
+    consulta = "SELECT img_vestido FROM productos WHERE id_producto = %s"
+    cursor.execute(consulta, (producto_id,))
+    resultado = cursor.fetchone()
+    if resultado:
+        img_path = resultado[0]
+
+        consulta = "DELETE FROM productos WHERE id_producto = %s"
+        cursor.execute(consulta, (producto_id,))
+        mydb.commit()
+
+        if os.path.exists(img_path):
+            os.remove(img_path)
+
+    cursor.close()
+    mydb.close()
+
+
+def obtener_vestido_por_id(id):
+    mydb = get_connection()
+    cursor = mydb.cursor()
+
+    consulta = "SELECT * FROM productos WHERE id_producto = %s"
+    cursor.execute(consulta, (id,))
+
+    vestido = cursor.fetchone()
+
+    cursor.close()
+    mydb.close()
+
+    return vestido
+
+
+def actualizar_vestido(id_producto, nombre, talla, color, categoria, descripcion, precio, estado, cantidad, img_vestido):
+    try:
+        with get_connection() as mydb:
+            cursor = mydb.cursor()
+
+            consulta = "SELECT img_vestido FROM productos WHERE id_producto = %s"
+            cursor.execute(consulta, (id_producto,))
+            resultado = cursor.fetchone()
+            if resultado:
+                img_path = resultado[0]
+
+                sql = "UPDATE productos SET nombre = %s, talla = %s, color = %s, categoria = %s, descripcion = %s, precio = %s, estado = %s, cantidad = %s, img_vestido = %s WHERE id_producto = %s"
+                values = (nombre, talla, color, categoria, descripcion, precio, estado, cantidad, img_vestido, id_producto)
+
+                cursor.execute(sql, values)
+                mydb.commit()
+
+                if os.path.exists(img_path) and img_path != img_vestido:
+                    os.remove(img_path)
+
+    except Exception as e:
+        print("Error al actualizar el vestido:", e)
+
+def actualizar_vestido_en_db(id, nombre, talla, color, categoria, descripcion, precio, estado, cantidad, img_path):
+    try:
+        with get_connection() as mydb:
+            cursor = mydb.cursor()
+
+            sql = "UPDATE productos SET nombre = %s, talla = %s, color = %s, categoria = %s, descripcion = %s, precio = %s, estado = %s, cantidad = %s, img_vestido = %s WHERE id_producto = %s"
+            values = (nombre, talla, color, categoria, descripcion, precio, estado, cantidad, img_path, id)
+
+            cursor.execute(sql, values)
+            mydb.commit()
+
+    except Exception as e:
+        print("Error al actualizar el vestido:", e)
+
+def obtener_vestido_por_id(id):
+    try:
+        with get_connection() as mydb:
+            cursor = mydb.cursor()
+
+            consulta = "SELECT * FROM productos WHERE id_producto = %s"
+            cursor.execute(consulta, (id,))
+
+            vestido = cursor.fetchone()
+
+            return vestido
+
+    except Exception as e:
+        print("Error al obtener el vestido:", e)
+        return None
--- app/views/vestidos_views.py
+++ app/views/vestidos_views.py
@@ -1,9 +1,9 @@
 import os
 from flask import Blueprint,render_template, url_for, request, redirect
-from models.db import get_connection
 from flask_login import login_required
-from models.vestidos import agregar_vestido, obtener_todos_los_vestidos
-from flask_wtf.csrf import CSRFProtect
+from werkzeug.utils import secure_filename
+from models.db import get_connection
+from models.vestidos import agregar_vestido, obtener_todos_los_vestidos,eliminar_vestido_por_id, obtener_vestido_por_id
 
 vestidos_views = Blueprint('vestidos_views', __name__)
 
@@ -47,3 +47,58 @@ def agregar_vestidos():
         return redirect(url_for('vestidos_views.ver_vestidos'))
 
     return render_template('users/vestidos/resgistro_vestidos.html')
+
+@vestidos_views.route('/vestidos/borrar/<int:id>/', methods=['POST'])
+@login_required
+def borrar_vestido(id):
+    eliminar_vestido_por_id(id)
+    return redirect(url_for('vestidos_views.ver_vestidos'))
+
+import os
+
+@vestidos_views.route('/vestidos/actualizar/<int:id>/', methods=['GET', 'POST'])
+@login_required
+def actualizar_vestido(id):
+    vestido = obtener_vestido_por_id(id)
+
+    if not vestido:
+        return render_template('error.html', message='El vestido no existe.')
+
+    if request.method == 'POST':
+        nombre = request.form.get('nombre')
+        talla = request.form.get('talla')
+        color = request.form.get('color')
+        categoria = request.form.get('categoria')
+        descripcion = request.form.get('descripcion')
+        precio = request.form.get('precio')
+        img_vestido = request.files.get('img_vestido')
+
+        img_path_anterior = vestido[9]
+
+        if img_vestido and img_vestido.filename != '':
+            if os.path.exists(img_path_anterior):
+                os.remove(img_path_anterior)
+
+            filename = secure_filename(img_vestido.filename)
+            img_path_nueva = os.path.join('static', 'img', 'vestidos', filename)
+            img_vestido.save(img_path_nueva)
+        else:
+            img_path_nueva = img_path_anterior
+
+        img_path_nueva = img_path_nueva.replace("\\", "/")
+        try:
+            with get_connection() as mydb:
+                cursor = mydb.cursor()
+
+                sql = "UPDATE productos SET nombre = %s, talla = %s, color = %s, categoria = %s, descripcion = %s, precio = %s, img_vestido = %s WHERE id_producto = %s"
+                values = (nombre, talla, color, categoria, descripcion, precio, img_path_nueva, id)
+
+                cursor.execute(sql, values)
+                mydb.commit()
+
+        except Exception as e:
+            print("Error al actualizar el vestido:", e)
+
+        return redirect(url_for('vestidos_views.ver_vestidos'))
+
+    return render_template('users/vestidos/actualizar_vestido.html', vestido=vestido)
